ffa3a86e09fd67e303c25b6eed6b0e14c6cb61f2
frank.cornelis <frank.cornelis@c38af997-75a8-35d5-7a30-6478fcb916de>
2011-07-12 10:22:05 +0000

    menu controller

---
+++ eid-archetype-seam/src/main/resources/archetype-resources/__rootArtifactId__-admin-control-ejb/src/main/java/admin/MenuControllerBean.java
@@ -0,0 +1,41 @@
+package ${package}.admin;
+
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+
+import org.jboss.ejb3.annotation.LocalBinding;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Destroy;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+
+@Stateful
+@Name("${admin-control-seam-components-prefix}MenuController")
+@LocalBinding(jndiBinding = "${admin-control-jndi-pattern-prefix}MenuControllerBean")
+@Scope(ScopeType.SESSION)
+public class MenuControllerBean implements MenuController {
+
+	@Logger
+	private Log log;
+
+	private String selectedChild;
+
+	@Override
+	public String getSelectedChild() {
+		return this.selectedChild;
+	}
+
+	@Override
+	public void setSelectedChild(String selectedChild) {
+		this.selectedChild = selectedChild;
+	}
+
+	@Remove
+	@Destroy
+	@Override
+	public void destroy() {
+		this.log.debug("destroy");
+	}
+}
--- eid-archetype-seam/src/main/resources/archetype-resources/__rootArtifactId__-control-ejb/src/main/java/AuthenticatorBean.java
+++ eid-archetype-seam/src/main/resources/archetype-resources/__rootArtifactId__-control-ejb/src/main/java/AuthenticatorBean.java
@@ -1,28 +1,17 @@
 package ${package};
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
-import javax.crypto.Mac;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
 import org.jboss.ejb3.annotation.LocalBinding;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.international.LocaleSelector;
 import org.jboss.seam.log.Log;
 import org.jboss.seam.security.Credentials;
-import org.joda.time.DateTime;
-import org.joda.time.Years;
 
 import be.fedict.eid.idp.common.AttributeConstants;
 import be.fedict.eid.idp.common.saml2.AuthenticationResponse;
@@ -31,8 +20,6 @@ import be.fedict.eid.idp.common.saml2.AuthenticationResponse;
 @Stateless
 @LocalBinding(jndiBinding = "${control-jndi-pattern-prefix}AuthenticatorBean")
 public class AuthenticatorBean implements Authenticator {
-
-	public static final String HMAC_ALGO = "HmacSHA1";
 
 	@Logger
 	private Log log;
