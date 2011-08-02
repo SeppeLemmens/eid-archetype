eae6780adabf29a5530763a1fdbb07e25abff056
frank.cornelis <frank.cornelis@c38af997-75a8-35d5-7a30-6478fcb916de>
2011-08-02 14:04:13 +0000

    JavaEE6: admin WAR refactoring

--- eid-archetype-javaee6/src/main/resources/archetype-resources/__rootArtifactId__-admin-war/src/main/java/admin/Constants.java
+++ eid-archetype-javaee6/src/main/resources/archetype-resources/__rootArtifactId__-admin-war/src/main/java/admin/Constants.java
@@ -2,7 +2,7 @@ package ${package}.admin;
 
 public class Constants {
 
-	public static final String JNDI_PREFIX = "${admin-control-cdi-components-prefix}";
+	public static final String CDI_PREFIX = "${admin-control-cdi-components-prefix}";
 
 	private Constants() {
 		super();
