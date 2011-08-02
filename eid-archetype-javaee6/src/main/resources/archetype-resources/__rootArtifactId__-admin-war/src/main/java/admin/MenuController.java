20b428919d8987f57365c2b6092648ce38cc4a33
frank.cornelis <frank.cornelis@c38af997-75a8-35d5-7a30-6478fcb916de>
2011-08-02 14:13:42 +0000

    JavaEE6: compile fixes

--- eid-archetype-javaee6/src/main/resources/archetype-resources/__rootArtifactId__-admin-war/src/main/java/admin/MenuController.java
+++ eid-archetype-javaee6/src/main/resources/archetype-resources/__rootArtifactId__-admin-war/src/main/java/admin/MenuController.java
@@ -1,6 +1,5 @@
 package ${package}.admin;
 
-import javax.ejb.Remove;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -12,18 +11,11 @@ public class MenuController implements Serializable {
 
 	private String selectedChild;
 
-	@Override
 	public String getSelectedChild() {
 		return this.selectedChild;
 	}
 
-	@Override
 	public void setSelectedChild(String selectedChild) {
 		this.selectedChild = selectedChild;
-	}
-
-	@Override
-	@Remove
-	public void destroy() {
 	}
 }
