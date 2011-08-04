2046b829a63e1876feba297c4e470f819edd8713
frank.cornelis <frank.cornelis@c38af997-75a8-35d5-7a30-6478fcb916de>
2011-08-04 14:57:06 +0000

    JavaEE6: display admin photo

--- eid-archetype-javaee6/src/main/resources/archetype-resources/__rootArtifactId__-admin-war/src/main/java/admin/BelgianIdentityCardProducer.java
+++ eid-archetype-javaee6/src/main/resources/archetype-resources/__rootArtifactId__-admin-war/src/main/java/admin/BelgianIdentityCardProducer.java
@@ -1,13 +1,17 @@
 package ${package}.admin;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.security.cert.X509Certificate;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 
 import be.fedict.eid.applet.service.Identity;
 
+@Named(Constants.CDI_PREFIX + "eIDController")
 public class BelgianIdentityCardProducer {
 
 	@Inject
@@ -26,5 +30,11 @@ public class BelgianIdentityCardProducer {
 		X509Certificate authCert = (X509Certificate) this.httpSession
 				.getAttribute("eid.certs.authn");
 		return authCert;
+	}
+	
+	public void createPhoto(OutputStream outputStream, Object data)
+			throws IOException {
+		byte[] photo = (byte[]) this.httpSession.getAttribute("eid.photo");
+		outputStream.write(photo);
 	}
 }
